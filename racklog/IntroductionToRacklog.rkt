00dca4615b00f2ae94309ef299d490b3a9b23281
William Turkett <5298611+turketwh@users.noreply.github.com>
2024-11-21 17:43:48 -0500

    Create IntroductionToRacklog.rkt

---
+++ racklog/IntroductionToRacklog.rkt
@@ -0,0 +1,54 @@
+#lang racket
+(require racklog)
+
+; space relations
+(define %star
+  (%rel ()
+        [('sun)]
+        [('sirius)]
+        [('vega)]))
+
+(define %orbits
+  (%rel ()
+        [('mercury 'sun)]
+        [('venus 'sun)]
+        [('earth 'sun)]
+        [('mars 'sun)]
+        [('jupiter 'sun)]
+        [('saturn 'sun)]
+        [('uranus 'sun)]
+        [('neptune 'sun)]
+        [('moon 'earth)]
+        [('phobos 'mars)]
+        [('deimos 'mars)]))
+        
+; space predicate
+(define %spacePredicate 
+   (%rel (p) 
+   [(p) 
+    (%orbits p 'sun)]))
+
+; family parent relation
+(define %parent
+  (%rel ()
+        [('Curtis 'William)]
+        [('Margie 'William)]
+        [('Curtis 'Carol)]
+        [('Margie 'Carol)]
+        [('Ernie 'Jennifer)]
+        [('Carol 'Jennifer)]
+        [('William 'Billy)]
+        [('William 'Laura)]
+        [('Mary 'Billy)]
+        [('Mary 'Laura)]
+        [('Laura 'Virginia)]
+        [('Laura 'Miles)]
+        ))
+
+; family mystery relationship
+(define %mysteryRelationship 
+    (%rel (x y z) 
+     [(x y) 
+      (%parent z x) 
+      (%parent z y) 
+      (%/= x y)]))
