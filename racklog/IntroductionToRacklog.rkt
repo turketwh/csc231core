c72e991c75904ea33ea69b04ff335d3edc4d565e
William Turkett <5298611+turketwh@users.noreply.github.com>
2025-11-01 17:48:54 -0400

    Reordered definitions

--- racklog/IntroductionToRacklog.rkt
+++ racklog/IntroductionToRacklog.rkt
@@ -1,6 +1,31 @@
 #lang racket
 (require racklog)
 
+; family parent relation
+(define %parent
+  (%rel ()
+        [('Curtis 'William)]
+        [('Margie 'William)]
+        [('Curtis 'Carol)]
+        [('Margie 'Carol)]
+        [('Ernie 'Jennifer)]
+        [('Carol 'Jennifer)]
+        [('William 'Billy)]
+        [('William 'Laura)]
+        [('Mary 'Billy)]
+        [('Mary 'Laura)]
+        [('Laura 'Virginia)]
+        [('Laura 'Miles)]
+        ))
+
+; family mystery relationship
+(define %mysteryRelationship 
+    (%rel (x y z) 
+     [(x y) 
+      (%parent z x) 
+      (%parent z y) 
+      (%/= x y)]))
+
 ; space relations
 (define %star
   (%rel ()
@@ -28,27 +53,3 @@
    [(p) 
     (%orbits p 'sun)]))
 
-; family parent relation
-(define %parent
-  (%rel ()
-        [('Curtis 'William)]
-        [('Margie 'William)]
-        [('Curtis 'Carol)]
-        [('Margie 'Carol)]
-        [('Ernie 'Jennifer)]
-        [('Carol 'Jennifer)]
-        [('William 'Billy)]
-        [('William 'Laura)]
-        [('Mary 'Billy)]
-        [('Mary 'Laura)]
-        [('Laura 'Virginia)]
-        [('Laura 'Miles)]
-        ))
-
-; family mystery relationship
-(define %mysteryRelationship 
-    (%rel (x y z) 
-     [(x y) 
-      (%parent z x) 
-      (%parent z y) 
-      (%/= x y)]))
